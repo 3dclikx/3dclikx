3493184ccada683325b69d55d35da8f560e656b6
airrakeshkumarsharma <airrakeshkumarsharma@gmail.com>
2019-01-10 22:05:12 +0530

    fet request is added

--- routes/index.js
+++ routes/index.js
@@ -10,5 +10,26 @@ router.get('/index', function(req, res, next) {
 router.get('/3dproduct', function(req, res, next) {
   res.render('product');
 });
+router.get('/robotics', function(req, res, next) {
+  res.render('product2');
+});
+router.get('/about', function(req, res, next) {
+  res.render('about');
+});
+router.get('/contact', function(req, res, next) {
+  res.render('contact');
+});
+router.get('/help', function(req, res, next) {
+  res.render('help');
+});
+router.get('/terms', function(req, res, next) {
+  res.render('terms');
+});
+router.get('/faqs', function(req, res, next) {
+  res.render('faqs');
+});
+router.get('/privacy', function(req, res, next) {
+  res.render('privacy');
+});
 
 module.exports = router;
