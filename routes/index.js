4e2c2ea0e7a3b637b03cb36087477af82e8760d7
airrakeshkumarsharma <airrakeshkumarsharma@gmail.com>
2019-01-12 22:14:12 +0530

    email is added

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,8 @@ var dbs = monk('localhost:27017/3dclikx');
 var clikx = dbs.get("usersignin"); ///sign up data base
 var cutomeOrder = dbs.get("customeorder");  //custome order
 var email   = require('emailjs/email');
-var nodemailer = require('nodemailer');    // email package for sending email
+var nodemailer = require('nodemailer'); 
+// const xoauth2 = require('xoauth2');   // email package for sending email
 //var upload = require('express-fileupload');    // multipart request taking by the servervar 
 var multer = require('multer');
 /* GET home page. */
@@ -268,7 +269,41 @@ router.post('/customepost', upload.single('originalname'), function (req, res, n
 // ==============================custome end  ================================
 
 router.post("/forgotpassword", function(req, res){
-  console.log(req.body.email);
+  clikx.findOne({useremail : req.body.email}, function(err, doc){
+    if(doc)
+    {
+
+            
+             var transporter =  nodemailer.createTransport({
+                                service: 'gmail',
+                                 host: 'smtp.gmail.com',
+                                port: 465,
+                                secure: true, // use SSL
+                                auth: {
+                                    user: '',
+                                    pass: ''
+                                }
+                            });
+            var msg = "<html><body class=\"\" style=\"\/* background-color: #7acbee; *\/font-family:sans-serif;-webkit-font-smoothing:antialiased;font-size:14px;line-height:1.4;margin:0;padding:0;-ms-text-size-adjust:100%;-webkit-text-size-adjust:100%;\"><table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" class=\"body\" style=\"border-collapse:separate;mso-table-lspace:0pt;mso-table-rspace:0pt;background-color: #7acbee;width:100%;\"><tbody><tr><td style=\"font-family:sans-serif;font-size:14px;vertical-align:top;\">&nbsp;<\/td><td class=\"container\" style=\"font-family:sans-serif;font-size:14px;vertical-align:top;display:block;max-width:580px;padding:10px;width:580px;Margin:0 auto !important;\"><div class=\"content\" style=\"box-sizing:border-box;display:block;Margin:0 auto;max-width:580px;padding:10px;\"><!-- START CENTERED WHITE CONTAINER --><span class=\"preheader\" style=\"color:transparent;display:none;height:0;max-height:0;max-width:0;opacity:0;overflow:hidden;mso-hide:all;visibility:hidden;width:0;\"><\/span><table class=\"main\" style=\"border-collapse:separate;mso-table-lspace:0pt;mso-table-rspace:0pt;background:#fff;border-radius:3px;width:100%;\"><tbody><tr><td class=\"wrapper\" style=\"font-family:sans-serif;font-size:14px;vertical-align:top;box-sizing:border-box;padding:20px;\"><table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" style=\"border-collapse:separate;mso-table-lspace:0pt;mso-table-rspace:0pt;width:100%;\"><tbody><tr><td style=\"font-family:sans-serif;font-size:14px;vertical-align:top;\"><p style=\"font-family:sans-serif;font-size:14px;font-weight:normal;margin:0;Margin-bottom:15px;margin-left: 210px;\"><img src='http:\/\/www.digitalgujarat.gov.in\/images\/logo.png' width=\"80px\"><\/p><p style=\"font-family:sans-serif;font-size:14px;font-weight:normal;margin:0;Margin-bottom:15px;\">Hi "+req.body.email+", <br><br>Your Account Has Been Created successfully<\/b><\/p><table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" class=\"btn btn-primary\" style=\"border-collapse:separate;mso-table-lspace:0pt;mso-table-rspace:0pt;box-sizing:border-box;width:100%;\"><tbody><tr><td align=\"left\" style=\"font-family:sans-serif;font-size:14px;vertical-align:top;\"><center><table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" style=\"border-collapse:separate;mso-table-lspace:0pt;mso-table-rspace:0pt;width:100%;width:auto;\"><tbody><tr><td style=\"font-family:sans-serif;font-size:14px;vertical-align:top;background-color:#ffffff;border-radius:5px;text-align:center;background-color:;\"><\/td><\/tr><\/tbody><\/table><\/center><\/td><\/tr><\/tbody><\/table><p style=\"font-family:sans-serif;font-size:14px;font-weight:normal;margin:0;\"><\/p><center><b><\/b><\/center><p><\/p><p style=\"font-family:sans-serif;font-size:14px;font-weight:normal;margin:0;Margin-bottom:15px;\">Thanks &amp; Regards,<br>Team#Six.<\/p><\/td><\/tr><\/tbody><\/table><\/td><\/tr><\/tbody><\/table><!-- START FOOTER --><div class=\"footer\" style=\"clear:both;padding-top:10px;text-align:center;width:100%;\"><table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" style=\"border-collapse:separate;mso-table-lspace:0pt;mso-table-rspace:0pt;width:100%;\"><tbody><tr><td class=\"content-block\" style=\"font-family:sans-serif;font-size:14px;vertical-align:top;color: #000000;font-size:12px;text-align:center;\"><span class=\"apple-link\" style=\"color: #000000;font-size:12px;text-align:center;\">Govt of Gujarat<\/span><br>This email is system generated, please do not respond to this email.<\/td><\/tr><tr><td class=\"content-block powered-by\" style=\"font-family:sans-serif;font-size:14px;vertical-align:top;color: black;font-size:12px;text-align:center;\">Powered by Team#six. <br><a href='#' target=\"_blank\" style=\"color:white;\" ><\/a><\/td><\/tr><\/tbody><\/table><\/div><!-- END FOOTER --><!-- END CENTERED WHITE CONTAINER --><\/div><\/td><td style=\"font-family:sans-serif;font-size:14px;vertical-align:top;\">&nbsp;<\/td><\/tr><\/tbody><\/table><\/body><\/html>";            
+            var mailOptions = {
+            from: '"SIH " <myhub@aditya.ac.in>', // sender address
+            to:"airrakeshkumarsharma@gmail.com", // list of receivers
+            subject: 'Caste Verification',
+            //text: msg // plaintext body
+            html: msg // html body
+            }
+
+            // send the message and get a callback with an error or details of the message that was sent
+            transporter.sendMail(mailOptions, function(error, response){
+            if(error){
+            console.log("Email could not sent due to error: "+error);
+            }else{
+            console.log("Email has been sent successfully");
+            }
+            });
+    }
+  });
+    
 });
 
 // ==============================Fogot password ===============================
